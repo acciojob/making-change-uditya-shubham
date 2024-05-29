0d6876b84de232ff97a33ca85c93220dc163888a
Uditya Kumar <shubhamkumaar2001@gmail.com>
2024-05-30 01:09:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const makeChange = (c) => {
-  // your name here
+  let change = {
+    q: 0,
+    d: 0,
+    n: 0,
+    p: 0
+  };
+
+  // Calculate the number of quarters
+  change.q = Math.floor(c / 25);
+  c = c % 25;
+
+  // Calculate the number of dimes
+  change.d = Math.floor(c / 10);
+  c = c % 10;
+
+  // Calculate the number of nickels
+  change.n = Math.floor(c / 5);
+  c = c % 5;
+
+  // The remainder is the number of pennies
+  change.p = c;
+
+  return change;
 };
 
-// Do not the change the code below
-const c = prompt("Enter c: ");
+// Do not change the code below
+const c = parseInt(prompt("Enter c: "));
 alert(JSON.stringify(makeChange(c)));
